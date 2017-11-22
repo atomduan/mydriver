d8479d17b9aff811c61da5c269228b5f05d64839
duanjuntao [段俊涛] <duanjuntao@xiaomi.com>
2017-11-22 10:43:20 +0800

    mod major dev number to pass

--- helloworld/helloworld_mod.c
+++ helloworld/helloworld_mod.c
@@ -30,7 +30,14 @@
 #include <linux/string.h>
 #include "helloworld_mod_ioctl.h"
 
-#define MAJOR_NUM 250
+/*
+ * cat /proc/devices (the major_num is not precise here) 
+ * or ll /dev (the major_num is precise here) to check whether the MAJOR_NUM has been ocupied.
+ * for example: the 250 MAJOR_NUM are ocupied by watchdog device(module?).
+ *
+ * check module: lsmod, ll /dev, cat /proc/devices
+ */
+#define MAJOR_NUM 255
 #define MINOR_NUM 0
 #define IN_BUF_LEN 256
 #define OUT_BUF_LEN 512
@@ -71,7 +78,7 @@ struct file_operations helloworld_mod_fops =
 static int 
 helloworld_mod_open(struct inode *inode, struct file *pfile)
 {
-    printk("+helloworld_mod_open()!/n");
+    printk("+helloworld_mod_open()!\n");
     spin_lock(&spin);
     if(open_count)
     {
@@ -80,70 +87,70 @@ helloworld_mod_open(struct inode *inode, struct file *pfile)
     }
     open_count++;
     spin_unlock(&spin);
-    printk("-helloworld_mod_open()!/n");
+    printk("-helloworld_mod_open()!\n");
     return 0;
 }
 
 static int 
 helloworld_mod_release(struct inode *inode, struct file *pfile)
 {
-    printk("+helloworld_mod_release()!/n");
+    printk("+helloworld_mod_release()!\n");
     open_count--;
-    printk("-helloworld_mod_release()!/n");
+    printk("-helloworld_mod_release()!\n");
     return 0;
 }
 
 static ssize_t 
 helloworld_mod_read(struct file *pfile, char *user_buf, size_t len, loff_t *off)
 {
-    printk("+helloworld_mod_read()!/n");
+    printk("+helloworld_mod_read()!\n");
 
     if(down_interruptible(&sem))
     {
         return -ERESTARTSYS; 
     }
     memset(outbuffer, 0, OUT_BUF_LEN);
-    printk("    +switch()/n");
+    printk("    +switch()\n");
     switch(langtype)
     {
         case english:
-            printk("        >in case: english/n");
+            printk("        >in case: english\n");
             sprintf(outbuffer, "Hello! %s.", inbuffer);
             break;
         case chinese:
-            printk("        >in case: chinese/n");
+            printk("        >in case: chinese\n");
             sprintf(outbuffer, "你好！ %s.", inbuffer);
             break;
         case pinyin:
-            printk("        >in case: pinyin/n");
+            printk("        >in case: pinyin\n");
             sprintf(outbuffer, "ni hao! %s.", inbuffer);
             break;
         default:
-            printk("        >in case: default/n");
+            printk("        >in case: default\n");
             break;
     }
-    printk("    -switch()/n");
+    printk("    -switch()\n");
     if(copy_to_user(user_buf, outbuffer, len))
     {
         up(&sem);
         return -EFAULT;
     }
     up(&sem);
-    printk("-helloworld_mod_read()!/n");
+    printk("-helloworld_mod_read()!\n");
     return 0;
 }
 
 static ssize_t 
 helloworld_mod_write(struct file *pfile, const char *user_buf, size_t len, loff_t *off)
 {
-    printk("+helloworld_mod_write()!/n");
+    printk("+helloworld_mod_write()!\n");
     if(down_interruptible(&sem))
     {
         return -ERESTARTSYS;
     }
     if(len > IN_BUF_LEN)
     {
-        printk("Out of input buffer/n");
+        printk("Out of input buffer\n");
         return -ERESTARTSYS;
     }
     if(copy_from_user(inbuffer, user_buf, len))
@@ -152,7 +159,7 @@ helloworld_mod_write(struct file *pfile, const char *user_buf, size_t len, loff_
         return -EFAULT;
     }
     up(&sem);    
-    printk("-helloworld_mod_write()!/n");
+    printk("-helloworld_mod_write()!\n");
     return 0;
 }
 
@@ -160,29 +167,29 @@ static long
 helloworld_mod_ioctl(struct file *pfile, unsigned int cmd, unsigned long arg)
 {
     int err = 0;
-    printk("+helloworld_mod_ioctl()!/n");
-    printk("    +switch()/n");
+    printk("+helloworld_mod_ioctl()!\n");
+    printk("    +switch()\n");
     switch(cmd)
     {
         case HELLOWORLD_IOCTL_RESETLANG:
-            printk("        >in case: HELLOWORLD_IOCTL_RESETLANG/n");
+            printk("        >in case: HELLOWORLD_IOCTL_RESETLANG\n");
             langtype = english;
             break;
         case HELLOWORLD_IOCTL_GETLANG:
-            printk("        >in case: HELLOWORLD_IOCTL_GETLANG/n");
+            printk("        >in case: HELLOWORLD_IOCTL_GETLANG\n");
             err = copy_to_user((int *)arg, &langtype, sizeof(int));
             break;
         case HELLOWORLD_IOCTL_SETLANG:
-            printk("        >in case: HELLOWORLD_IOCTL_SETLANG/n");
+            printk("        >in case: HELLOWORLD_IOCTL_SETLANG\n");
             err = copy_from_user(&langtype,(int *)arg, sizeof(int));
             break;
         default:
-            printk("        >in case: default/n");
+            printk("        >in case: default\n");
             err = ENOTSUPP;
             break;
     }
-    printk("    -switch()/n");
-    printk("-helloworld_mod_ioctl()!/n");
+    printk("    -switch()\n");
+    printk("-helloworld_mod_ioctl()!\n");
     return err;
 }
 
@@ -190,13 +197,13 @@ static int __init
 helloworld_mod_init(void)
 {
     int result;
-    printk("+helloworld_mod_init()!/n");
+    printk("+helloworld_mod_init()!\n");
     devnum = MKDEV(MAJOR_NUM, MINOR_NUM);
     result = register_chrdev_region(devnum, 1, modname);
 
     if(result < 0)
     {
-        printk("helloworld_mod : can't get major number!/n");
+        printk("helloworld_mod : can't get major number! devnum is %u, result is %d,\n", devnum, result);
         return result;
     }    
 
@@ -209,7 +216,7 @@ helloworld_mod_init(void)
     helloworld_class = class_create(THIS_MODULE, classname);
     if(IS_ERR(helloworld_class))
     {
-        printk("Failed at class_create().Please exec [mknod] before operate the device/n");
+        printk("Failed at class_create().Please exec [mknod] before operate the device\n");
     }
     else
     {
@@ -221,21 +228,21 @@ helloworld_mod_init(void)
     inbuffer = (char *)kmalloc(IN_BUF_LEN, GFP_KERNEL);
     outbuffer = (char *)kmalloc(OUT_BUF_LEN, GFP_KERNEL);
     sema_init(&sem, 1);
-    printk("-helloworld_mod_init()!/n");
+    printk("-helloworld_mod_init()!\n");
     return 0;
 }
 
 static void __exit 
 helloworld_mod_exit(void)
 {
-    printk("+helloworld_mod_exit!/n");
+    printk("+helloworld_mod_exit!\n");
     kfree(inbuffer);
     kfree(outbuffer);
     cdev_del(&helloworld_cdev);
     device_destroy(helloworld_class, devnum);
     class_destroy(helloworld_class);
     unregister_chrdev_region(devnum, 1);
-    printk("-helloworld_mod_exit!/n");
+    printk("-helloworld_mod_exit!\n");
     return ;
 }
 
